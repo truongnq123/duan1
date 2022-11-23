7116d4f3dc6c6efe2c242cecbda36903026b3663
Cter05 <ductruongtm03@gmail.com>
2022-11-23 13:10:44 +0700

    lay code cua tien ve

--- control/left.php
+++ control/left.php
@@ -1,7 +1,7 @@
 <div class="sidenav">
                 <div class="menu">
                     <ul>
-                        <li ><i class="fa fa-home"></i><a href="././index.php"> Home</a></li>
+                        <li ><i class="fa fa-home"></i><a href="./home.php"> Home</a></li>
                         <li ><i class="fa-solid fa-user"></i><a href="index.php?act=adddm">Danh Muc</a></li>
                         <li ><i class="fa-solid fa-user"><a href="index.php?act=addsp"></i>San Pham</a></li>
                         <li ><i class="fa-solid fa-user"></i><a href="index.php?act=addkh">Khach Hang + Admin</a></li>
