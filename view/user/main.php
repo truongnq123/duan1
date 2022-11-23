62dda61da6796233d0892deac8ca02d43a5013c8
tienlee <vuletien2003@gmail.com>
2022-11-23 12:55:01 +0700

    tienlee

--- view/user/main.php
+++ view/user/main.php
@@ -1,5 +1,7 @@
 <div class="main">
+        <div class="full-body">
             <div class="body">
+            
                 <div class="title_slide">
                     <h1>THE BEST LAPTOP </h1>
                     <p>Enhance your experience</p>
@@ -28,6 +30,8 @@
                             class="fa fa-angle-right"></i></button>
 
                 </div>
+            </div>
+            </div>
             </div>
         </div>
 
@@ -59,145 +63,98 @@
             <div class="list_product_popular">
                 <!--------------------------------------------Phan them php cho san pham--------------------------------------------------------->
                 <div class="snip1583">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample68.jpg" alt="sample68" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a><a href=""></a></div>
-
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
                     <figcaption>
-                        <h3>Wisteria Ravenclaw</h3>
-                        <div class="price">$15.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583 "><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample77.jpg"
-                        alt="sample77" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ursula Gurnmeister</h3>
-                        <div class="price">$89.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
                     </figcaption>
                 </div>
                 <div class="snip1583">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample68.jpg" alt="sample68" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
                     <figcaption>
-                        <h3>Wisteria Ravenclaw</h3>
-                        <div class="price">$15.00</div>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
+                    </figcaption>
+                </div><div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
+                    <figcaption>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
                     </figcaption>
                 </div>
-                <div class="snip1583 "><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample68.jpg"
-                        alt="sample77" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
+                <div class="snip1583">
+                    <div class="img-zoom">
+                    <img src="https://hanoicomputer.net/wp-content/uploads/2022/08/Laptop-Dell-Inspiron-3501-3692BLK1..jpg" alt="sample68" />
+                    </div>
+                    
                     <figcaption>
-                        <h3>Ursula Gurnmeister</h3>
-                        <div class="price">$89.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
-                    </figcaption>
-                </div>
-                <div class="snip1583"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample71.jpg"
-                        alt="sample71" />
-                    <div class="icons"><a><i class="fa fa-shopping-cart"></i></a><a><i class="fa fa-star"></i></a><a><i
-                                class="fa fa-share-alt"></i></a></div>
-                    <figcaption>
-                        <h3>Ingredia Nutrisha</h3>
-                        <div class="price">$19.00</div>
+                        <h5>Laptop Dell Vostro 3510 (P112F002BBL) (i5 1135G7 8GBRAM/512GB SSD/MX350 2G/15.6 inch FHD/Win11/Office HS21/Đen)</h5>
+                        <div class="price">₫18.290.000</div>
                     </figcaption>
                 </div>
             </div>
         </div>
-        <div class="banner_footer">
-            <img src="./src/img/banner_footer.jpg" alt="">
-        </div>
-        <div class="service_pay">
-            <div class="service">
-                <div class="icon">
-                    <i class="fa-regular fa-comments" aria-hidden="true" ></i>
-                </div>
-                <h2>24/7 Customer Suport</h2>
-                <p>Have a question? Call a or chat online.</p>
-            </div>
-            <div class="service">
-                <div class="icon">
-                    <i class="fa-regular fa-credit-card" aria-hidden="true"></i>
-                </div>
-                <h2>Money Back Guarantee</h2>
-                <p>Pay with the world’s and secure payment methods.</p>
-            </div>
-            <div class="service">
-                <div class="icon"><i class="fa-solid fa-hand-holding-dollar" aria-hidden="true"></i></div>
-                <h2>Shop With Confidence</h2>
-                <p>Our Buyer Protection purchasefrom click to delivery.</p>
-            </div>
-            <div class="service">
-                <div class="icon">
-                    <i class="fa-solid fa-rotate-left" aria-hidden="true"></i>
-                </div>
-                <h2>90 Days Return</h2>
-                <p>Passage Of Lorem Ipsum To Be Embarrassing</p>
-            </div>
-        </div>
+       
