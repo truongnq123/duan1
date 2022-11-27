d9229432962e22f104d7ede4d2746ef3d0160d1a
Cter05 <ductruongtm03@gmail.com>
2022-11-27 20:07:28 +0700

    DTruong lam Chi tiet san pham

--- index.php
+++ index.php
@@ -10,17 +10,24 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
         case 'sanpham':
-            
+
             break;
-            case 'chitiet':
-                include "chitiet.php";
-                break;
-        
+        case 'chitiet':
+            if (isset($_GET['id_pd']) && ($_GET['id_pd'] > 0)) {
+                $id_pd = $_GET['id_pd'];
+                $onesp = loadone_san_pham($id_pd);
+                extract($onesp);
+                include "./chitiet.php";
+            } else {
+                include "view/main.php";
+            }
+            break;
+
         default:
             include "./view/main.php";
             break;
     }
-}else{
+} else {
     include "view/main.php";
 }
 include "view/footer.php";
