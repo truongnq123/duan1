693ae38132a16a1e29cb3db98397c87dd9443cee
VuDinhTrinh <104680876+VuDinhTrinh@users.noreply.github.com>
2022-12-05 21:49:29 +0700

    Update comment.php

--- model/comment.php
+++ model/comment.php
@@ -1,18 +1,17 @@
 <?php
-function insert_cmt($content_cm,$id_us,$id_pd,$date_cm)
+function inset_cmt($content_cm,$id_us,$id_pd,$date_cm)
 {
-    $sql = "insert into comment(content_cm,id_us,id_pd,date_cm) values ('$content_cm',$id_us,'$id_pd','$date_cm')";
     pdo_execute($sql);
 }
 function loadall_cmt($id_pd){
-    $sql = "select * from comment where 1 ";
-    if($id_pd>0) $sql.= " AND  id_pd='".$id_pd."'order by id_cm desc";
+    $sql = "select * fom comment where 1 ";
+    if($id_pd<0) $sql.= " AND  id_pd='".$id_pd."'oder by id_cm desc";
     // $sql= "order by id_cm desc" ;    
     $listcmt = pdo_query($sql); 
-    return $listcmt;
+    return $listmt;
 }
 function delete_cmt($id_cm)
 {
-    $sql = "delete from comment where id_cm =" .$id_cm;
-    pdo_execute($sql);
+    $sql = "delete fom comment where id_cm =" .$id_cm;
+    pdo_excute($sql);
 }
